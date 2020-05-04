e042751ae89f7dd1bd0dc32b4ca104d4c26adc32
cmarcus-nhs <60224748+cmarcus-nhs@users.noreply.github.com>
2020-05-04 10:34:34 +0100

    add cds and fhir server to docker-compose (#3)
    * add cds and fhir server to docker-compose
    
    * try and sleep
    
    * increase timout to 30 seconds
    
    * try agaian with timout
    
    * revert
    
    * don't sleep
    
    * add run script and add to ci
    
    * fix running script
    
    * tidying up

---
+++ scripts/run-cactus.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+healthcheck() {
+    until [[ $(curl -s -o /dev/null -w %{http_code} $1) == "200" ]] ; do
+        echo "healthcheck: $1 attempt $attempt_counter"
+        if [ ${attempt_counter} -eq ${max_attempts} ];then
+        echo "Max attempts reached"
+        exit 1
+        fi
+        attempt_counter=$(($attempt_counter+1))
+        sleep $retry_wait
+    done
+    echo "$1 ready"
+}
+
+echo "Planting a cactus..."
+
+docker-compose up -d
+
+attempt_counter=1
+retry_wait=5
+max_attempts=18 #90 seconds
+
+echo "waiting for cdss..."
+healthcheck "http://localhost:8080/fhir/metadata"
+echo "waiting for fhir server..."
+healthcheck "http://localhost:8084/fhir/metadata"
+echo "waiting for ems..."
+healthcheck "http://localhost:8083/fhir/metadata"
